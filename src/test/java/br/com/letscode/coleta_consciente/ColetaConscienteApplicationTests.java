86149295b32448af5dd85850a6f730d924850b35
nepster <gabrielnepster1@gmail.com>
2021-08-11 13:44:43 -0300

    first commit

---
+++ src/test/java/br/com/letscode/coleta_consciente/ColetaConscienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.coleta_consciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetaConscienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
