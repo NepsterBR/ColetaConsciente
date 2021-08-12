d5febe4dccca9ef64682dc167f169438167b495d
nepster <gabrielnepster1@gmail.com>
2021-08-12 16:06:03 -0300

    implementando regra de negocio

--- src/main/java/br/com/letscode/coleta_consciente/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.letscode.coleta_consciente.repository;
-
-import br.com.letscode.coleta_consciente.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
---
+++ src/main/java/br/com/letscode/coleta_consciente/resquest/PontoColetaRequets.java
@@ -0,0 +1,53 @@
+package br.com.letscode.coleta_consciente.resquest;
+
+import br.com.letscode.coleta_consciente.entity.PontoColeta;
+import br.com.letscode.coleta_consciente.entity.enuns.Estados;
+import br.com.letscode.coleta_consciente.entity.enuns.TipoEmpresa;
+import br.com.letscode.coleta_consciente.entity.enuns.TipoResiduo;
+import br.com.letscode.coleta_consciente.repository.ClienteRepository;
+import br.com.letscode.coleta_consciente.repository.PontoColetaRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PontoColetaRequets {
+
+    public List<PontoColeta> assimilar(Estados estados, TipoEmpresa tipoEmpresa, TipoResiduo tipoResiduo,
+                          float preco, int cpf, PontoColetaRepository pontoColetaRepository,
+                                       ClienteRepository clienteRepository) {
+
+        var lista = pontoColetaRepository.findAll();
+        var cliente = clienteRepository.findById(cpf);
+        PontoColeta pontoResquest = new PontoColeta();
+        List<PontoColeta> requetsList = new ArrayList<>();
+
+        for (int i = 0; i < lista.toArray().length; i++) {
+            pontoResquest.setCnpj(lista.get(i).getCnpj());
+            pontoResquest.setEmail(lista.get(i).getEmail());
+            pontoResquest.setEstado(lista.get(i).getEstado());
+            pontoResquest.setCidade(lista.get(i).getCidade());
+            pontoResquest.setEndereco(lista.get(i).getEndereco());
+            pontoResquest.setNomeSocial(lista.get(i).getNomeSocial());
+            pontoResquest.setPreco(lista.get(i).getPreco());
+            pontoResquest.setTipoEmpresa(lista.get(i).getTipoEmpresa());
+            pontoResquest.setTipoResiduo(lista.get(i).getTipoResiduo());
+            pontoResquest.setTotalPagar(lista.get(i).getPreco()*cliente.get().getQuantidade());
+            requetsList.add(pontoResquest);
+        }
+
+        return requetsList.stream().filter(l -> l.getPreco() >= preco)
+                .filter(l -> l.getTipoEmpresa().equals(tipoEmpresa))
+                .filter(l -> l.getTipoResiduo().equals(tipoResiduo))
+                .filter(l -> l.getEstado().equals(estados))
+                .collect(Collectors.toList());
+    }
+
+}
